4bfe5c665eee9cbf03f4b5e1ee7906289994563d
amandallago <amandslago@gmail.com>
2025-09-22 15:47:12 -0300

    Create python1.py

---
+++ python1.py
@@ -0,0 +1,35 @@
+# Primeiro contato #
+
+print('Olá, Mundo')
+
+print(7+4)
+
+print('Olá' , 5)
+
+nome = 'Amanda'
+idade = '18'
+peso = '54.3'
+
+print(nome, idade, peso)
+
+nome = input ('Qual é o seu nome?')
+idade = input ('Quantos anos você tem?')
+peso = input ('Qual é o seu peso?')
+
+print(nome, idade, peso)
+
+# Desafio 1 #
+nome = input('Qual é o seu nome?')
+print('Boas-vindas', nome,'!')
+
+# Desafio 2 #
+dia = input('Que dia você nasceu?')
+mes = input('Em que mês você nasceu?')
+ano = input('Qual ano você nasceu?')
+print('Você nasceu no dia',dia, 'de',mes, 'de',ano)
+
+# Desafio 3 #
+num1 = input('Digite o primeiro número')
+num2 = input('Digite o segundo número')
+soma = (num1 + num2)
+print('A soma dos números',num1,'e',num2,'é',soma)
